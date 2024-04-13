f787ed9b05f15964933ac05f7a92fa2d175eb647
aaronjnc <aaronkingnc@gmail.com>
2024-04-13 13:22:21 -0400

    Continue enemy movement, start animations

--- Assets/Scripts/EnemyScripts/EnemyManager.cs
+++ Assets/Scripts/EnemyScripts/EnemyManager.cs
@@ -2,9 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+struct WaveInformation
+{
+    public int waveNumber;
+    public int enemyCount;
+    public GameObject[] enemyPrefabs;
+    public Sprite flowerSprite;
+}
+
 public class EnemyManager : Singleton<EnemyManager>
 {
-    [SerializeField]
-    private GameObject enemyPrefab;
 
+    [SerializeField]
+    private List<WaveInformation> waves;
+
+    private List<GameObject> activeEnemies;
+
+    private GameObject player;
+
+    int currentWave = 0;
+
+    private void Start()
+    {
+        activeEnemies = new List<GameObject>();
+        player = PlayerMovement.Instance.gameObject;
+    }
+
+    private void NewWave()
+    {
+        WaveInformation wave = waves[currentWave];
+        for (int i = 0; i < wave.enemyCount; i++)
+        {
+            GameObject newEnemy = Instantiate(wave.enemyPrefabs[UnityEngine.Random.Range(0, wave.enemyPrefabs.Length - 1)]);
+            newEnemy.GetComponent<EnemyMovement>().MoveTo(player);
+            activeEnemies.Add(newEnemy);
+        }
+    }
+
+    public void RemoveEnemy(GameObject killedEnemy)
+    {
+        activeEnemies.Remove(killedEnemy);
+        if (activeEnemies.Count == 0)
+        {
+            currentWave++;
+            NewWave();
+        }
+    }
 }
--- Assets/Scripts/EnemyScripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyScripts/EnemyMovement.cs
@@ -1,25 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 [RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(NavMeshAgent))]
 public class EnemyMovement : MonoBehaviour
 {
+    private NavMeshAgent agent;
     private Rigidbody rb;
     private PlayerMovement playerMovement;
     [SerializeField]
     private float enemySpeed;
     [SerializeField]
     private float enemyMinDistance;
+    private GameObject enemyTarget;
     // Start is called before the first frame update
     void Start()
     {
         playerMovement = PlayerMovement.Instance;
+        agent = GetComponent<NavMeshAgent>();
+        agent.stoppingDistance = enemyMinDistance;
+        agent.speed = enemySpeed;
+        MoveTo(playerMovement.gameObject);
     }
 
-    // Update is called once per frame
-    void FixedUpdate()
+    public void MoveTo(GameObject target)
     {
-        
+        enemyTarget = target;
+        agent.SetDestination(target.transform.position);
+    }
+
+    private void FixedUpdate()
+    {
+        if (enemyTarget.transform.position != agent.destination)
+        {
+            agent.SetDestination(enemyTarget.transform.position);
+        }
     }
 }
