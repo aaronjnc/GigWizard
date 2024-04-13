6990a3b6a000db2dc6797bc2a62c600c84cb7d45
aaronjnc <aaronkingnc@gmail.com>
2024-04-13 16:04:24 -0400

    Working spawn points

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,7 +9,7 @@ public class GameManager : Singleton<GameManager>
     private List<SpawnPoint> closedSpawnPoints = new List<SpawnPoint>();
 
     [SerializeField]
-    private int spawnPointCloseTime;
+    private float spawnPointCloseTime;
 
     protected override void Awake()
     {
@@ -21,25 +21,13 @@ public class GameManager : Singleton<GameManager>
     {
         if (openSpawnPoints.Count == 0)
         {
-            WaitForPoint();
+            openSpawnPoints.AddRange(closedSpawnPoints.ToArray());
+            closedSpawnPoints.Clear();
         }
         int randSpawnPoint = UnityEngine.Random.Range(0, openSpawnPoints.Count - 1);
         SpawnPoint spawnPoint = openSpawnPoints[randSpawnPoint];
         openSpawnPoints.RemoveAt(randSpawnPoint);
-        CloseSpawnPoint(spawnPoint);
-        return spawnPoint.transform.position;
-    }
-
-    async void CloseSpawnPoint(SpawnPoint spawnPoint)
-    {
         closedSpawnPoints.Add(spawnPoint);
-        await Task.Delay(spawnPointCloseTime);
-        closedSpawnPoints.Remove(spawnPoint);
-        openSpawnPoints.Add(spawnPoint);
-    }
-
-    IEnumerator WaitForPoint()
-    {
-        yield return new WaitForSeconds(spawnPointCloseTime);
+        return spawnPoint.transform.position;
     }
 }
