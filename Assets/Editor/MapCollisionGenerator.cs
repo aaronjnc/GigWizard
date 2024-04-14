a03fab3905307e5e9336beb365f34bed835ed0ea
Aaron <aaronkingnc@gmail.com>
2024-04-14 17:38:12 -0400

    Change pre-processor

--- Assets/Editor/MapCollisionGenerator.cs
+++ Assets/Editor/MapCollisionGenerator.cs
@@ -7,15 +7,59 @@ using UnityEngine.Tilemaps;
 [CustomEditor(typeof(Tilemap))]
 public class MapCollisionGenerator : Editor
 {
+    private const string parentObjName = "World Colliders";
+
     public override void OnInspectorGUI()
     {
         base.OnInspectorGUI();
         Tilemap map = (Tilemap)target;
-        GameObject cubeCollider = null;
-        cubeCollider = (GameObject)EditorGUILayout.ObjectField ("Cube Collider", cubeCollider, typeof(GameObject), false);
         if(GUILayout.Button("Generate Colliders"))
         { 
-            
+            GameObject prevParent = GameObject.Find(parentObjName);
+            if (prevParent != null)
+            {
+                DestroyImmediate(prevParent);
+            }
+            GameObject parentObj = new GameObject(parentObjName);
+            for (int x = map.cellBounds.min.x; x < map.cellBounds.max.x; x++)
+            {
+                for (int y = map.cellBounds.min.y; y < map.cellBounds.max.y; y++)
+                {
+                    for (int z =  map.cellBounds.min.z; z < map.cellBounds.max.z; z++)
+                    {
+                        if (map.HasTile(new Vector3Int(x, y, z)))
+                        {
+                            bool bSurrounded = true;
+                            for (int xMod = -1; xMod <= 1; xMod++)
+                            {
+                                for (int yMod = -1; yMod <= 1; yMod++)
+                                {
+                                    if (!map.HasTile(new Vector3Int(x + xMod, y + yMod, z)))
+                                    {
+                                        bSurrounded = false;
+                                        break;
+                                    }
+                                }
+                                if (!bSurrounded)
+                                {
+                                    break;
+                                }
+                            }
+                            if (bSurrounded)
+                            {
+                                continue;
+                            }
+                            Vector3 spawnLoc = map.CellToWorld(new Vector3Int(x, y, z));
+                            spawnLoc.y = 1;
+                            spawnLoc += new Vector3(.5f, 0, .5f);
+                            GameObject newCube = new GameObject(x + " " + y + " " + z);
+                            newCube.transform.position = spawnLoc;
+                            newCube.AddComponent<BoxCollider>();
+                            newCube.transform.SetParent(parentObj.transform);
+                        }
+                    }
+                }
+            }
         }   
     }
 }
