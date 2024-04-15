69231c619a540e6c9a2e215dee4dbd96dcf18b44
juuu-jiii <52075702+juuu-jiii@users.noreply.github.com>
2024-04-15 19:47:14 -0400

    Update AudioManager to handle familiar sounds
    For more flexibility?

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -13,13 +13,22 @@ public enum MusicTypes
 
 public class AudioManager : Singleton<AudioManager>
 {
+    public enum Familiars
+    {
+        Goat,
+        Dragon
+    }
+
     private Dictionary<SpellTypes, Sound> _spellClipsToAudioClipDictionary;
     private Dictionary<MusicTypes, Sound> _musicClipsToAudioClipDictionary;
+    private Dictionary<Familiars, Sound> _familiarToAudioClipDictionary;
 
     [SerializeField] private Sound _lightSpell;
     [SerializeField] private Sound _waterSpell;
     [SerializeField] private Sound _plantSpell;
     [SerializeField] private Sound _protectSpell;
+    [SerializeField] private Sound _goatSound;
+    [SerializeField] private Sound _dragonSound;
     [SerializeField] private Sound _menuMusic;
     [SerializeField] private Sound _wanderingMusic;
     [SerializeField] private Sound _badStuffMusic;
@@ -37,6 +46,11 @@ public class AudioManager : Singleton<AudioManager>
     {
         _bgmAudioSource.clip = _musicClipsToAudioClipDictionary[musicType].clip;
         _bgmAudioSource.Play();
+    }
+
+    public void PlayFamiliarSound(Familiars familiar)
+    {
+        _sfxAudioSource.PlayOneShot(_familiarToAudioClipDictionary[familiar].clip);
     }
 
     public void PlaySpellSound(SpellTypes spellType)
@@ -70,6 +84,12 @@ public class AudioManager : Singleton<AudioManager>
             { MusicTypes.MenuMusic, _menuMusic },
             { MusicTypes.WanderingMusic, _wanderingMusic },
             { MusicTypes.BadStuff, _badStuffMusic }
+        };
+
+        _familiarToAudioClipDictionary = new Dictionary<Familiars, Sound>
+        {
+            { Familiars.Goat, _goatSound },
+            { Familiars.Dragon, _dragonSound }
         };
 
         PlayMusic(MusicTypes.WanderingMusic);
